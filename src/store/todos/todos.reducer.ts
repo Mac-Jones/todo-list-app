5e5cdb0db21da2fbdabf0a9a488dc85f3d67e1fc
macjonessoriano <sorianomacjones@gmail.com>
2023-07-10 13:12:51 +0800

    change something

--- src/store/todos/todos.reducer.ts
+++ src/store/todos/todos.reducer.ts
@@ -1,17 +1,11 @@
 import { AnyAction } from 'redux';
 import { Todo, TODOS_ACTION_TYPES } from './todos.types';
-// import {
-// 	updateAddTodo,
-// 	updateToggleTodo,
-// 	updateInputChange,
-// 	toDelete,
-// } from './todos.action';
 
 export type TodosState = {
 	readonly todos: Todo[];
 };
 
-const TODOS_INITIAL_STATE: TodosState = {
+export const TODOS_INITIAL_STATE: TodosState = {
 	todos: [],
 };
 
@@ -19,17 +13,6 @@ export const todosReducer = (
 	state = TODOS_INITIAL_STATE,
 	action: AnyAction
 ) => {
-	// if (
-	// 	updateToggleTodo.match(action) ||
-	// 	updateAddTodo.match(action) ||
-	// 	updateInputChange.match(action) ||
-	// 	toDelete.match(action)
-	// ) {
-	// 	return { ...state, todos: action.payload };
-	// }
-
-	// return state;
-
 	switch (action.type) {
 		case TODOS_ACTION_TYPES.UPDATE_TODO: {
 			return { ...state, todos: action.payload };
